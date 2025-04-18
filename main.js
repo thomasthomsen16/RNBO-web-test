e4836ae7a6b34799d08ec3c84aa17d623d25912a
Thomas T <35918236+thomasthomsen16@users.noreply.github.com>
2025-04-18 14:14:13 +0200

    Clean up removed RNBO files

--- main.js
+++ main.js
@@ -14,11 +14,31 @@ document.getElementById("start").addEventListener("click", async () => {
             patcher: patcher
         });
 
+        // Create a gain node and set the gain value
         const gainNode = audioCTX.createGain();
         gainNode.gain.value = 0.5; // Set the gain value to 0.5
+
+
+        // Create a delay node and feedback node
+        const delayNode = audioCTX.createDelay();
+        delayNode.delayTime.value = 100; // Set the delay time to 0.5 seconds
+        const feedbackNode = audioCTX.createGain();
+        feedbackNode.gain.value = 0.5; // Set the feedback gain to 0.5
+        // Connect the delay node to the feedback node
+        delayNode.connect(feedbackNode);
+        feedbackNode.connect(delayNode); 
+        delayNode.connect(gainNode); // Connect the delay node to the gain node
+
         device.node.connect(gainNode); // Connect the device node to the gain node
+        device.node.connect(delayNode); // Connect the device node to the delay node
         gainNode.connect(audioCTX.destination); // Connect the gain node to the audio context destination
     }
 });
 
-
+document.getElementById("play").addEventListener("click", () => { 
+    const trig = device.parametersById.get("trig");
+    trig.value=1;
+    setTimeout(() => {
+        trig.value=0;
+    }, 20);
+})
